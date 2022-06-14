44dcdbfdfc3a6ab901fd32ab45f1dac3d6af9edc
z421 <starggewars1977@gmail.com>
2022-06-14 13:57:55 -0400

    updated program to refresh images folder

--- finalProgram.py
+++ finalProgram.py
@@ -14,6 +14,8 @@ from dotenv import load_dotenv
 load_dotenv()
 reddit = praw.Reddit(client_id=os.getenv('CLIENT_ID'), client_secret=os.getenv('CLIENT_SECRET'), user_agent=os.getenv('USER_AGENT'))
 
+import shutil
+
 def getImages():
     query = queryInput.get(1.0, "end-1c")
     if query == '': query='Kenobi'
@@ -46,6 +48,8 @@ def getImages():
             if re.match(imagePattern, post.url):
                 allImagePosts.append(post.url)
 
+    if os.path.exists('images/'):
+        shutil.rmtree('images', ignore_errors=True)
     if not os.path.exists('images/'):
         os.makedirs('images')
     for imageURL in allImagePosts:
